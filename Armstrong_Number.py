3eb26c09d33e8726de5e26fce44594725eefb27e
mccarthy-silva <63144785+mccarthy-silva@users.noreply.github.com>
2020-04-27 22:54:47 +0300

    Update Armstrong_Number.py

--- Armstrong_Number.py
+++ Armstrong_Number.py
@@ -1,16 +1,16 @@
-a = input("Enter a number please \n")
-if  a.isdigit() == True :
+number = input("Enter a number please \n")
+if  number.isdigit() == True :
     c = 0
-    for b in a:
+    for b in number:
         b = int(b)
-        c += b**len(a)
-    a = int(a)
-    if c == a:
-        print ("{} is an amstrong number.".format(a))
+        c += b**len(number)
+    number = int(number)
+    if c == number:
+        print ("{} is an amstrong number.".format(number))
     else: 
-        print ("{} is not an amstrong number.".format(a))
-elif a[0] == "-" :
+        print ("{} is not an amstrong number.".format(number))
+elif number[0] == "-" :
     print("Please enter a positive number")
-elif "." in a or "," in a: 
+elif "." in number or "," in number: 
     print("Please enter an integer number")
 else : print("Do not use any entries other than numeric values")
